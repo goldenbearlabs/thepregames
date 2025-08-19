4d638560949e9d8382cbc234570d82a72548394c
Luke Wismer <lukewismer77@gmail.com>
2025-08-18 19:51:55 -0700

    test deploy

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,10 +1,11 @@
-export default {
-    reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     transpilePackages: [
       "@thepregames/shared",
-      "@thepregames/game-engines",
       "@thepregames/ui",
+      "@thepregames/game-engines",
       "@thepregames/api-clients"
     ]
   };
+  export default nextConfig;
   
